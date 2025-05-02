7f5fd7f8f4514a6831dbb69fc416f27420d4cd57
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-05-02 11:44:33 -0300

    Screen modifications

--- screen.gd
+++ screen.gd
@@ -3,8 +3,9 @@ extends Contenedor
 class_name Screen
 
 func _ready() -> void:
-	get_viewport().size_changed.connect(set_screen)
 	super()
+	get_viewport().size_changed.connect(set_screen)
+	self.set_screen()
 
 
 func set_screen():
@@ -14,9 +15,21 @@ func set_screen():
 		self.set_real_size(Vector2(1152, 648)) 
 	else:
 		self.set_real_size(self.get_viewport().size)
-		self._layout.move_elements()
+		self._set_layout()
+	
+	for child in self.get_all_descendants(self):
+		if child is Contenedor:
+			child._set_layout()
 
 
 func editor_settings() -> void:
-	self.set_screen()
 	super()
+	self.set_screen()
+
+
+func get_all_descendants(node: Node) -> Array:
+	var descendants = []
+	for child in node.get_children():
+		descendants.append(child)
+		descendants += get_all_descendants(child)
+	return descendants
