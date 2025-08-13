eece266041c146ec9cdd5c425fd896bf1adbae0d
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-08-13 14:23:50 -0300

    new: space_between

--- src/contenedor/layout/types/sausage.gd
+++ src/contenedor/layout/types/sausage.gd
@@ -2,33 +2,51 @@ extends Layout
 class_name Sausage
 
 const VERTICAL = "vertical"
+const SPACE_BETWEEN = "space_between"
 
 var rows = {}
+
+var vertical: bool:
+	get():
+		return contenedor.layout.config[VERTICAL]
+var space_between_value: int:
+	get():
+		return contenedor.layout.config[SPACE_BETWEEN]
 
 ## [OVERWRITTED]
 func get_default_config() -> Dictionary:
 	var c = {}
 	c[VERTICAL] = false
+	c[SPACE_BETWEEN] = 0
 	return c
 
 
 ## [OVERWRITTED]
 func calculate_dimensions() -> void:
-	var vertical = contenedor.layout.config[VERTICAL]
 	var key = "y" if vertical else "x"
+	var contenedor_size = contenedor.get_area().size
+	var spaces_ordered = self.get_spaces_ordered()
+	var space_between = (contenedor_size[key] / 100) * space_between_value
+	
+	contenedor_size[key] -= space_between * (spaces.size() - 1)
+	var magnitude_unit = contenedor_size[key] / 100.0
+	
 	var aux = 0.0
-	var contenedor_area = contenedor.get_area()
+	var area = Rect2(Vector2.ZERO, contenedor_size)
 	
-	for space: SausageSpace in self.get_spaces_ordered():
-		var available_area = contenedor_area
-		var m = (space.fill / 100.0) * available_area.size[key]
-		available_area.position[key] += aux
-		available_area.size[key] = m
-		self.set_ente_area(space.name, available_area)
-		aux += m
+	for i in range(spaces.size()):
+		var last = i == spaces.size() -1
+		var space: SausageSpace = spaces_ordered[i]
+		var magnitude = space.fill * magnitude_unit
+		
+		area.position[key] = aux
+		area.size[key] = magnitude
+		
+		self.set_ente_area(space.name, area)
+		aux += magnitude + (space_between if !last else 0.0)
 	
-	contenedor_area.size[key] = aux
-	contenedor.real_size = contenedor_area.size
+	contenedor_size[key] = aux 
+	contenedor.real_size = contenedor_size
 
 
 ## [OVERWRITTED]
