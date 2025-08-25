fe76b29cf063936de497ad05b80c9ca8f971e36b
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-08-25 19:50:19 -0300

    mod: screen_size

--- src/contenedor/variants/screen.gd
+++ src/contenedor/variants/screen.gd
@@ -2,6 +2,8 @@
 extends ScrollContainer
 class_name Screen
 
+@export var screen_size: Vector2 = Vector2.ZERO
+
 var resize_timer = Timer.new()
 var last_size = Vector2.ZERO
 
@@ -12,12 +14,11 @@ func _ready() -> void:
 	resize_timer.one_shot = true
 	resize_timer.connect("timeout", set_viewport_area)
 	self.add_child(resize_timer)
-	get_tree().get_root().size_changed.connect(
-		func():
-			resize_timer.start()
-	)
+	var root = get_tree().get_root()
+	root.size_changed.connect(resize_timer.start)
 	
 	await get_tree().process_frame
+	
 	self.set_viewport_area()
 
 
@@ -28,9 +29,8 @@ func _notification(what: int) -> void:
 
 func set_viewport_area() -> void:
 	global_position = Vector2.ZERO
-	var _size_ = get_parent_area_size()
-	
+	var _size_ = get_parent_area_size() if screen_size.is_zero_approx() or !Engine.is_editor_hint() else screen_size
+	size = _size_
 	for child in self.get_children():
 		if child is Ente:
-			custom_minimum_size = _size_
 			child.set_area(Rect2(global_position, _size_))
