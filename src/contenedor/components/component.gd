4db021224c88c7ebd56e8b4994382e2bedf2567e
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-08-25 19:48:55 -0300

    fix: text update

--- src/contenedor/components/component.gd
+++ src/contenedor/components/component.gd
@@ -7,9 +7,9 @@ var aux_children: Array = [] ## TODO: Rethink this, but its needed...
 
 ## [OVERWRITTEN] From: Ente
 func handle_resize() -> void:
-	super()
 	layout.spaces = self.get_layout_spaces()
 	layout.config = self.get_layout_config()
+	super()
 
 
 ## [OVERWRITTE] Get children to add to contenedor.
