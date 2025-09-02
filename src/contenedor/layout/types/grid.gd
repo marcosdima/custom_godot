215fd6503752e5eef2cc757f92b5f616890a7cc5
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-02 16:59:06 -0300

    new: auto row and columns count

--- src/contenedor/layout/types/grid.gd
+++ src/contenedor/layout/types/grid.gd
@@ -3,13 +3,14 @@ class_name Grid
 
 const ROWS = "rows"
 const COLUMNS = "columns"
-
+const AUTO = "auto_row_column"
 
 ## [OVERWRITTED]
 func get_default_config() -> Dictionary:
 	var s = super()
 	s[ROWS] = 1
 	s[COLUMNS] = 1
+	s[AUTO] = true
 	return s
 
 
@@ -19,8 +20,12 @@ func calculate_dimensions() -> void:
 	var indexed_spaces = {}
 	
 	# Check actual rows and columns.
-	var rows_count = contenedor.layout.config[Grid.ROWS]
-	var columns_count = contenedor.layout.config[Grid.COLUMNS]
+	var config_rows_count = contenedor.layout.config.get(ROWS, 1)
+	var config_columns_count = contenedor.layout.config.get(COLUMNS, 1)
+	var auto_row_column = contenedor.layout.config.get(AUTO, true)
+	
+	var columns_count = config_columns_count
+	var rows_count = config_rows_count
 	for space in self.get_spaces_ordered():
 		columns_count = max(columns_count, int(space.column + 1))
 		rows_count = max(rows_count, int(ceil(space.row + space.row_span)))
@@ -29,7 +34,10 @@ func calculate_dimensions() -> void:
 		if !indexed_spaces.has(key):
 			indexed_spaces.set(key, space)
 	
-	var cell_size = self.get_cell_size(rows_count, columns_count)
+	var cell_size = self.get_cell_size(
+		rows_count if auto_row_column else config_rows_count,
+		columns_count if auto_row_column else config_columns_count,
+	)
 	
 	# Initialice rows dictionary.
 	var rows = {}
@@ -41,7 +49,9 @@ func calculate_dimensions() -> void:
 	for row in range(rows_count):
 		# Acc variables...
 		var acc_position = rows[row]
-		var max_row_high = 0.0
+		var max_row_high = cell_size.y
+		var blank_spaces = 0
+		var aux = 0.0
 		
 		for column in range(columns_count):
 			var key = self.get_key(row, column)
@@ -50,15 +60,25 @@ func calculate_dimensions() -> void:
 			
 			# If row-column space exists...
 			if space:
+				# Update acc_position if there were blank spaces.
+				if blank_spaces > 0:
+					acc_position.x += real_cell_size.x * blank_spaces
+					blank_spaces = 0
+				
 				# Get cell real size.
 				var span = self.get_span(space)
 				real_cell_size = cell_size * span
 				
 				# Save ente relative area.
 				areas[space.name] = Rect2(acc_position, real_cell_size)
-			
-			acc_position.x += real_cell_size.x
-			max_row_high = min(cell_size.y, real_cell_size.y)
+				
+				# Update max_row_high (TODO: This actually does not work, fix later)
+				max_row_high = min(max_row_high, real_cell_size.y)
+				
+				# Update x position.
+				acc_position.x += real_cell_size.x
+			else:
+				blank_spaces += 1
 		
 		acc_position.y = max_row_high
 		rows[row] = acc_position
@@ -66,7 +86,7 @@ func calculate_dimensions() -> void:
 	var total_size = Vector2.ZERO
 	for size in rows.values():
 		total_size.x = max(total_size.x, size.x)
-		total_size.y = max(total_size.y, size.y)
+		total_size.y += size.y 
 	
 	var off_set = contenedor.get_start_offset(total_size)
 	for ente_key in areas:
