603401a548f796b3e34d05e743ef6bd428065a66
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-05 15:50:32 -0300

    mod: icon rework with Sprite2D

--- src/entes/icon.gd
+++ src/entes/icon.gd
@@ -25,61 +25,95 @@ enum DefaultIcons {
 	Wallet,
 }
 
-@export var icon_texture: CompressedTexture2D:
-	get():
-		if !icon_texture:
-			self.set_texture()
-		return icon_texture
 @export var color: Color:
 	set(value):
 		color = value
+		if sprite:
+			sprite.modulate = value
 		self.queue_redraw()
 @export var default: DefaultIcons = DefaultIcons.Play:
 	set(value):
 		default = value
 		self.set_texture()
 
-var rotate = 0
-
-const icons_path = "res://static/images/icons"
+var rotate: float
+var scale_factor: float
+var icon_texture: Texture2D
+var sprite: Sprite2D
+const icons_path = "res://static/images/icons/"
+const alternative_icons_path = "res://.godot/imported/"
 
 static func exists(icon: DefaultIcons) -> bool:
 	return FileAccess.file_exists(Icon.get_icon_path(icon))
 
 
 static func get_icon_path(icon: DefaultIcons) -> String:
-	return icons_path + "/" + DefaultIcons.find_key(icon).to_lower() + ".svg"
+	var key = DefaultIcons.find_key(icon).to_lower()
+	
+	if Alambre.is_computer():
+		return icons_path + key + '.svg'
+	
+	var dir = DirAccess.open(alternative_icons_path)
+	var target = ""
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir() and file_name.begins_with(key):
+				target = alternative_icons_path + file_name
+			file_name = dir.get_next()
+		dir.list_dir_end()
+	
+	return target
 
 
-func _init(def: DefaultIcons = DefaultIcons.Play) -> void:
+func _init(
+	def: DefaultIcons = DefaultIcons.Play,
+	scale_factor_: float = 0.7,
+	rotate_: float = 0,
+) -> void:
 	super()
+	
 	default = def
+	rotate = rotate_
+	scale_factor = scale_factor_
+	self.set_texture()
+	
+	## Creates a Sprite2D
+	sprite = Sprite2D.new()
+	sprite.texture = icon_texture
+	self.resize.connect(adapt_sprite)
+	self.add_child(sprite)
 
 
 func set_texture() -> void:
 	if Icon.exists(default):
-		var path = icons_path + "/" + DefaultIcons.find_key(default).to_lower() + ".svg"
+		var path = Icon.get_icon_path(default)
 		icon_texture = load(path)
-
-
-func _draw() -> void:
-	super()
-	
-	var contenedor_size = self.get_area().size
-	var texture_size = self.icon_texture.get_size()
-	
-	var scale_aux = min(contenedor_size.x / texture_size.x, contenedor_size.y / texture_size.y)
-	var final_size = texture_size * scale_aux * 0.7
-	var position_aux = (contenedor_size - final_size) / 2
-	
-	var angle = deg_to_rad(self.rotate)
-	var center = position_aux + final_size / 2  
-	
-	draw_set_transform(center, angle, Vector2(1, 1))
-	draw_texture_rect(self.icon_texture, Rect2(-final_size / 2, final_size), false, self.color)
-	draw_set_transform(Vector2.ZERO, 0, Vector2.ONE)
 
 
 func set_default(def: DefaultIcons) -> void:
 	default = def
-	self.handle_resize()
+	self.set_texture()
+	sprite.texture = icon_texture
+	self.adapt_sprite()
+
+
+func adapt_sprite() -> void:
+	if !sprite.texture:
+		return
+	sprite.modulate = color
+	
+	# Calculate proportional scale
+	var texture_size = sprite.texture.get_size()
+	var scale_aux = min(size.x / texture_size.x, size.y / texture_size.y)
+	var final_size = texture_size * scale_aux * 0.7
+	
+	# Set sprite scale
+	sprite.scale = final_size / texture_size
+	
+	# Center the sprite inside the container
+	sprite.position = (size - final_size) / 2 + final_size / 2
+	
+	# Optional rotation
+	sprite.rotation = deg_to_rad(rotate)
