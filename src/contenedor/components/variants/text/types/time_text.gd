dc3df3b36697e6b953e757e5f0f94d6361e2e077
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-07 22:07:25 -0300

    new: TextTime - Text in time format

---
+++ src/contenedor/components/variants/text/types/time_text.gd
@@ -0,0 +1,18 @@
+extends Text
+class_name TimeText
+
+var time: ClockTime
+var set_display: ClockTime.DisplayWith
+
+func _init(
+	time_: ClockTime = ClockTime.new(),
+	set_display_: ClockTime.DisplayWith = ClockTime.DisplayWith.Hours,
+) -> void:
+	time = time_
+	set_display = set_display_
+	content = time.as_string(set_display)
+
+
+func set_new_time(new_time: ClockTime) -> void:
+	time = new_time
+	self.update_content(time.as_string(set_display))
