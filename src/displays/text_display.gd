2b507e9c24ba5158487c5568e0187e783d60077b
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-22 19:20:17 -0300

    update resourses implementation

--- src/displays/text_display.gd
+++ src/displays/text_display.gd
@@ -4,11 +4,7 @@ class_name TextDisplay
 
 const TEXT = "Text"
 
-@export var text: TextResourse:
-	get():
-		if !text:
-			text = TextResourse.new()
-		return text
+@export var text: TextResourse = TextResourse.new()
 
 var text_display: Text
 
@@ -16,6 +12,6 @@ var text_display: Text
 func get_children_to_set() -> Array:
 	text_display = Text.new()
 	text_display.name = TEXT
-	text_display.set_from_resource(text)
 	text_display.color = color
+	text.apply(text_display)
 	return [text_display]
