bb21490b238c4a3cd74af998295afa69a2c24589
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-09-22 19:20:55 -0300

    mod: setup instead of _init

--- src/entes/contenedor/components/variants/text/char.gd
+++ src/entes/contenedor/components/variants/text/char.gd
@@ -3,23 +3,20 @@ class_name Char
 
 var value: String
 var pos: int
+var text: Text
 
 # Position.
 var row: int
 var column: int
 
-var text: Text
-var font = Font
-
 func _draw():
 	super()
-	
-	if font:
+	if text and text.font:
 		var font_color = text.color
 		var text_size = self.get_char_size()
 		
 		draw_char(
-			font,
+			text.font,
 			Vector2(0, text_size.y * 0.85), # TODO: Harcoded value.
 			value,
 			text.get_font_size(),
@@ -27,20 +24,25 @@ func _draw():
 		)
 
 
-func _init(t: Text, v: String, r: int, c: int, str_pos: int) -> void:
-	super()
-	text = t
-	font = t.font
-	column = c
-	row = r
-	value = v
-	name = str(r) + str(c)
+func setup(
+	text_: Text,
+	value_: String = '.',
+	row_: int = 0,
+	column_: int = 0,
+	str_pos: int = 0,
+) -> Char:
+	text = text_
+	value = value_
+	row = row_
+	column = column_
+	name = str(row) + str(column)
 	pos = str_pos
+	return self
 
 
 func get_char_size(f_size:int = 0) -> Vector2:
 	var aux = f_size if f_size else text.get_font_size()
-	return font.get_string_size(
+	return text.font.get_string_size(
 		value,
 		HORIZONTAL_ALIGNMENT_LEFT,
 		-1,
